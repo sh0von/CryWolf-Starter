3497681e816fea6feae120831cbb6f28f45120a5
shovon <minarshovon909@gmail.com>
2022-09-13 07:11:12 +0000

    First Commit

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: 'Sh0vons Blog',
-  description: 'A minimal blog website by Sh0von',
+  siteTitle: 'CryWolf Starter',
+  description: 'A simple ui gatsby blog starter template',
   author: 'sh0von',
   siteUrl: 'http://localhost:8000',
-  siteRepo: 'sh0von',
-  githubId: 'sh0von',
+  siteRepo: 'https://github.com/sh0von/CryWolf-Starter',
+  githubId: 'https://github.com/sh0von/',
   linkedInId: 'minar.svn',
   twitterId: 'minar.svn',
 };
